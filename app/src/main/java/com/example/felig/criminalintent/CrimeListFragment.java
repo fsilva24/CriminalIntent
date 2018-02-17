a500832c6a426078bf18ace8f8dddf64be715939
Felipe Silva <fsilva@mymail.mines.edu>
2018-02-17 00:44:58 -0700

    Implemented Frags

--- app/src/main/java/com/example/felig/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/felig/criminalintent/CrimeListFragment.java
@@ -37,12 +37,22 @@ public class CrimeListFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onResume(){
+        super.onResume();
+        updateUI();
+    }
+
     private void updateUI () {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
 
-        mAdapter = new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
+        if (mAdapter == null) {
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        } else {
+            mAdapter.notifyDataSetChanged();
+        }
     }
     private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
@@ -65,9 +75,10 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view){
-            Intent intent = new Intent(getActivity(), CrimeActivity.class);
+            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
             startActivity(intent);
         }
+
     }
 
     private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {

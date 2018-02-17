a776e437707065a5540e9e714a8ec1246073cf15
Felipe Silva <fsilva@mymail.mines.edu>
2018-02-16 21:32:44 -0700

    Implemented toasts

--- app/src/main/java/com/example/felig/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/felig/criminalintent/CrimeListFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.List;
 //import android.content.Intent;
@@ -42,7 +43,7 @@ public class CrimeListFragment extends Fragment {
         mAdapter = new CrimeAdapter(crimes);
         mCrimeRecyclerView.setAdapter(mAdapter);
     }
-    private class CrimeHolder extends RecyclerView.ViewHolder {
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         private Crime mCrime;
         private TextView mTitleTextView;
@@ -50,7 +51,7 @@ public class CrimeListFragment extends Fragment {
 
         public CrimeHolder(LayoutInflater inflater, ViewGroup parent){
             super(inflater.inflate(R.layout.list_item_crime, parent, false));
-
+            itemView.setOnClickListener(this);
             mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
             mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
         }
@@ -59,6 +60,11 @@ public class CrimeListFragment extends Fragment {
             mCrime = crime;
             mTitleTextView.setText(mCrime.getTitle());
             mDateTextView.setText(mCrime.getDate().toString());
+        }
+
+        @Override
+        public void onClick(View view){
+            Toast.makeText(getActivity(), mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();
         }
 
     }
@@ -88,5 +94,4 @@ public class CrimeListFragment extends Fragment {
             return mCrimes.size();
         }
     }
-
 }

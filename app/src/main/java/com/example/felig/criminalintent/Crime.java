4276c9dfd6139089d618a52f141a7e46a1f4b320
Felipe Silva <fsilva@mymail.mines.edu>
2018-02-16 19:57:42 -0700

    First commit. Crime List set up

---
+++ app/src/main/java/com/example/felig/criminalintent/Crime.java
@@ -0,0 +1,41 @@
+package com.example.felig.criminalintent;
+
+import java.util.UUID;
+import java.util.Date;
+
+/**
+ * Created by felig on 2/5/2018.
+ */
+
+public class Crime extends Object {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+    public UUID getId() {
+        return mId;
+    }
+    public String getTitle() {
+        return mTitle;
+    }
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+    public Date getDate() {
+        return mDate;
+    }
+    public void setDate(Date date) {
+        mDate = date;
+    }
+    public boolean isSolved() {
+        return mSolved;
+    }
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
---
+++ app/src/main/java/com/example/felig/criminalintent/CrimeListActivity.java
@@ -0,0 +1,15 @@
+package com.example.felig.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by felig on 2/15/2018.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
